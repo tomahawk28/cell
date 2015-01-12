6ecd83db079bb160ff5b589e325aa60b3a98ad25
JiHyuk Bok <ji-hyuk.bok@jdsu.com>
2015-01-12 11:02:27 +0900

    Add MIT License
    Add MIT License to each code

--- examples/cell_server.go
+++ examples/cell_server.go
@@ -1,3 +1,24 @@
+//JDSU CellAdvisor Web-Live Program
+//Copyright (C) 2015 Jihyuk Bok <tomahawk28@gmail.com>
+//
+//Permission is hereby granted, free of charge, to any person obtaining
+//a copy of this software and associated documentation files (the "Software"),
+//to deal in the Software without restriction, including without limitation
+//the rights to use, copy, modify, merge, publish, distribute, sublicense,
+//and/or sell copies of the Software, and to permit persons to whom the
+//Software is furnished to do so, subject to the following conditions:
+//
+//The above copyright notice and this permission notice shall be included
+//in all copies or substantial portions of the Software.
+//
+//THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+//EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+//OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+//IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+//DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+//TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+//OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
 package main
 
 import (
@@ -96,7 +117,7 @@ func Poller(done <-chan struct{}, in <-chan *Request, cell *cell.CellAdvisor, th
 }
 
 func NewRequest(command string, args map[string]string) *Request {
-	return &Request{command, args, make(chan []byte, 1)}
+	return &Request{command, args, make(chan []byte)}
 }
 
 func sendResult(done <-chan struct{}, pipe chan<- []byte, result []byte) {
